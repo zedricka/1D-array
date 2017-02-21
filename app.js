37fdacfd4df5a947b210aa06bec36bda5b360d03
creindle <creindle@hawaii.edu>
2017-02-21 10:37:48 -1000

    Update app.js

--- app.js
+++ app.js
@@ -9,16 +9,13 @@ function cars(){
 	var car3 = "Mazda";
 
 	//Create an array declaration named cars and set each car variable as the values.
-	var cars = [car1, car2, car3];
 
 	//Print the created array
-	console.log(cars);
 
 	//Print the array length
-	console.log(cars.length);
 
 	//Return the created array and the array length
-	return cars + cars.length;
+	return;
 }
 
 console.log(cars());
@@ -27,42 +24,20 @@ console.log(cars());
 * Demonstrates array declarions by storing parameters into a single array.
 *
 * Parameters 	food (string), drink (string), dessert (string)
-* Return 		An order as a complete sentence represented as a string
+* Return 	An order as a complete sentence represented as a string
 */
 function order(food, drink, dessert){
-	//Parameters stored in array called "order"
-	var order = [food, drink, dessert];
+	//Store the parameters in an array called "order"
 
-	//Print the entire array named "order"
-	console.log(order);
+	//Print the array named "order"
 	
 	//Print the length of the array named "order"
-	console.log(order.length);
 
-	return "I would like to order a " + order[0] + " " + order[1] + ", and a " + order[2] + ".";
+	//Return the order as a complete sentence represented as a string
+	return;
 }
 
 console.log(order("Burger", "root beer", "frosty"));
-
-/*
-* Uses for loop to iterate through array passed as a parameter
-* and replaces content at specified index within for loop
-*
-* Return 	classes (array, changed after iteration)
-*/
-function loopToChange(classes){
-	//Print the parameter that is being passed through the function
-	console.log(classes);
-
-	//Use a For loop to iterate through each value in the passed array
-	for (var i = 0; i < classes.length; i++){
-		console.log(classes[i]);
-		if (classes[i] === "Band"){
-			classes[i] = "Math";		
-		}
-	}
-	return classes;
-}
 
 var currentClasses = [
 	"Band", 
@@ -74,6 +49,21 @@ var currentClasses = [
 	"Web Development"
 ];
 
+/*
+* Uses for loop to iterate through array passed as a parameter
+* and replaces content at specified index within for loop
+*
+* Return 	classes (array, changed after iteration)
+*/
+function loopToChange(classes){
+	//Print the parameter that is being passed through the function
+
+	//Use a For loop to iterate through each value in the passed array
+	
+	//Return classes
+	return;
+}
+
 console.log(loopToChange(currentClasses));
 
 /*
@@ -84,16 +74,11 @@ console.log(loopToChange(currentClasses));
 */
 function arrayMethodToLoop(numbers){
 	//Print the parameter that is being passed through the function
-	console.log(numbers);
 
 	//Use the forEach() method to add 5 to each value in the passed array
-	numbers.forEach(function(number){
-		console.log(number);
-		number = number + 1;
-		console.log("New Numbers " + number)
-		numbers = numbers.sort();
-	})
-	return numbers;
+	
+	//Return the changed array
+	return;
 }
 
 console.log(arrayMethodToLoop([5, 7, 3, 4, 5]));
@@ -106,25 +91,17 @@ console.log(arrayMethodToLoop([5 + 1, 3]));
 */
 function moreArrayMethods(priorities){
 	//Print the parameter that is being passed through the function
-	console.log(priorities);
 
 	//Use the method sort() to sort your priorities
-	priorities.sort();
 
 	//Use the method indexOf() to replace "Not Going to Class" to "Going to Class"
-	var changeAtIndex = priorities.indexOf("Not Going to Class");
-	priorities[changeAtIndex] = "Going to Class";
-	console.log(priorities);
 
 	//Use the method pop() to remove the item at the end of the array
-	priorities.pop();
-	console.log(priorities);
 
 	//Use the method push() to add a priority that is important to the success of this class and don't forget to sort() them afterwards!
-	priorities.push('Take Breaks!');
-	priorities.sort();
 
-	return priorities;
+	//Return the changed array
+	return;
 }
 
 console.log(moreArrayMethods(["Zero Play", "Sleeping", "HW", "Work", "School", "Planning For Future", "Not Going to Class"]));
