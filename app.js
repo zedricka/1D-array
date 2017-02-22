0db59feef8173bb4bd61b648f52abb9bc1121410
zedricka <zedrick.ara@gmail.com>
2017-02-22 09:20:23 -1000

    ImConfused

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ function cars(){
 	var car1 = "Toyota";
 	var car2 = "Jeep Wrangler";
 	var car3 = "Mazda";
-
 	//Create an array declaration named cars and set each car variable as the values.
-
+	var cars = [car1, car2, car3];
 	//Print the created array
-
+	console.log(cars);
 	//Print the array length
-
+	console.log(cars.length);
 	//Return the created array and the array length
-	return;
+	return cars + " " + cars.length;
 }
 
 console.log(cars());
@@ -28,13 +27,13 @@ console.log(cars());
 */
 function order(food, drink, dessert){
 	//Store the parameters in an array called "order"
-
+		var order = [food, drink, dessert];
 	//Print the array named "order"
-	
+		console.log(order);
 	//Print the length of the array named "order"
-
+		console.log(order.length);
 	//Return the order as a complete sentence represented as a string
-	return;
+	return "Let's order: " + order[0] + " " + order[1] + " " + order[2];
 }
 
 console.log(order("Burger", "root beer", "frosty"));
@@ -57,10 +56,13 @@ var currentClasses = [
 */
 function loopToChange(classes){
 	//Print the parameter that is being passed through the function
-
+		console.log(classes);
 	//Use a For loop to iterate through each value in the passed array
-	
-	//Return classes
+		for (var i = classes.length - 1; i >= 0; i--) {
+			classes[i]
+		}
+
+	// Return classes
 	return;
 }
 
@@ -74,9 +76,22 @@ console.log(loopToChange(currentClasses));
 */
 function arrayMethodToLoop(numbers){
 	//Print the parameter that is being passed through the function
-
+		console.log(numbers);
 	//Use the forEach() method to add 5 to each value in the passed array
-	
+
+		numbers.forEach(function(curr) {
+				// console.log('my current value ', curr);
+				// console.log ('my index ', index);
+				// console.log ('my array ', arr);
+				var current = curr + 5;
+				// console.log(curr + 5);
+				var addme = numbers.push(current);
+				console.log(addme);
+			// var splices = numbers.splice (numbers.length, current);
+			// console.log(splices);
+				// var name = splices.push(curr + 5);
+				// console.log(name)
+		})
 	//Return the changed array
 	return;
 }
